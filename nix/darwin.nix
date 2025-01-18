667454378b84f47372c19aed4763b2cc9933caaa
michael <michaelcosj@proton.me>
2025-01-18 16:33:57 +0100

    adding home manager and some more refactoring

---
+++ nix/darwin.nix
@@ -0,0 +1,83 @@
+# Run darwin-help for documentation of all the configuration options
+{ self, ... }:
+
+{
+  environment.variables.HOMEBREW_NO_ANALYTICS = "1";
+  nixpkgs.hostPlatform = "x86_64-darwin";
+  security.pam.enableSudoTouchIdAuth = true;
+
+  homebrew = {
+    enable = true;
+    onActivation.autoUpdate = true;
+    onActivation.upgrade = true;
+    casks = [
+      "ghostty"
+      "zed"
+      "zen-browser"
+      "aldente"
+      "obsidian"
+      # "slack"
+      "raycast"
+      "karabiner-elements"
+      "localsend"
+      "stremio"
+    ];
+    brews = [
+      "mas"
+    ];
+    masApps = {
+      # "Yoink" = 457622435;
+    };
+  };
+
+  nix = {
+    gc.automatic = true;
+    settings = {
+      experimental-features = "nix-command flakes";
+      auto-optimise-store = true;
+    };
+  };
+
+  system = {
+    configurationRevision = self.rev or self.dirtyRev or null;
+    stateVersion = 5;
+
+    defaults = {
+      screencapture.location = "~/Pictures/Screenshots";
+
+      NSGlobalDomain = {
+        AppleInterfaceStyle = "Dark";
+        AppleShowAllExtensions = true;
+      };
+
+      dock = {
+        autohide = true;
+        show-recents = false;
+        autohide-time-modifier = 0.8;
+        mru-spaces = false;
+        showhidden = true;
+        persistent-apps = [
+          "/Applications/Zen Browser.app"
+          "/Applications/Slack.app"
+          "/Applications/Obsidian.app"
+          "/Applications/Ghostty.app"
+          "/Applications/Zed.app"
+          "/Applications/Postman.app"
+          "/Applications/Localsend.app"
+          "/System/Applications/Clock.app"
+          "/System/Applications/Calendar.app"
+          "/System/Applications/Mail.app"
+        ];
+      };
+
+      finder = {
+        AppleShowAllExtensions = true;
+        CreateDesktop = false;
+        ShowExternalHardDrivesOnDesktop = false;
+        ShowPathbar = true;
+        ShowStatusBar = true;
+        NewWindowTarget = "Home";
+      };
+    };
+  };
+}
--- nix/flake.nix
+++ nix/flake.nix
@@ -5,6 +5,8 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # Homebrew [https://github.com/zhaofengli/nix-homebrew]
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
@@ -27,18 +29,14 @@
     inputs@{
       self,
       nix-darwin,
-      nixpkgs,
       nix-homebrew,
-      homebrew-core,
-      homebrew-cask,
-      homebrew-bundle,
+      home-manager,
+      ...
     }:
     let
       configuration =
         { pkgs, ... }:
         {
-          # Run darwin-help for documentation of all the configuration options
-
           environment.systemPackages = [
             pkgs.neovim
             pkgs.nixd
@@ -53,32 +51,6 @@
             pkgs.jetbrains-mono
           ];
 
-          homebrew = {
-            enable = true;
-            onActivation.autoUpdate = true;
-            onActivation.upgrade = true;
-            casks = [
-              "ghostty"
-              "zed"
-              "zen-browser"
-              "aldente"
-              "obsidian"
-              # "slack"
-              "raycast"
-              "karabiner-elements"
-              "localsend"
-              "stremio"
-            ];
-            brews = [
-              "mas"
-            ];
-            masApps = {
-              # "Yoink" = 457622435;
-            };
-          };
-
-          environment.variables.HOMEBREW_NO_ANALYTICS = "1";
-
           programs = {
             zsh = {
               enable = true;
@@ -86,58 +58,6 @@
               enableSyntaxHighlighting = true;
               enableFzfCompletion = true;
               enableFzfHistory = true;
-            };
-          };
-
-          nixpkgs.hostPlatform = "x86_64-darwin";
-          security.pam.enableSudoTouchIdAuth = true;
-
-          nix = {
-            gc.automatic = true;
-            settings = { experimental-features = "nix-command flakes"; }
-          };
-
-
-          system = {
-            configurationRevision = self.rev or self.dirtyRev or null;
-            stateVersion = 5;
-
-            defaults = {
-              screencapture.location = "~/Pictures/Screenshots";
-
-              NSGlobalDomain = {
-                AppleInterfaceStyle = "Dark";
-                AppleShowAllExtensions = true;
-              };
-
-              dock = {
-                autohide = true;
-                show-recents = false;
-                autohide-time-modifier = 0.8;
-                mru-spaces = false;
-                showhidden = true;
-                persistent-apps = [
-                  "/Applications/Zen Browser.app"
-                  "/Applications/Slack.app"
-                  "/Applications/Obsidian.app"
-                  "/Applications/Ghostty.app"
-                  "/Applications/Zed.app"
-                  "/Applications/Postman.app"
-                  "/Applications/Localsend.app"
-                  "/System/Applications/Clock.app"
-                  "/System/Applications/Calendar.app"
-                  "/System/Applications/Mail.app"
-                ];
-              };
-
-              finder = {
-                AppleShowAllExtensions = true;
-                CreateDesktop = false;
-                ShowExternalHardDrivesOnDesktop = false;
-                ShowPathbar = true;
-                ShowStatusBar = true;
-                NewWindowTarget = "Home";
-              };
             };
           };
         };
@@ -148,6 +68,7 @@
       darwinConfigurations.macbook = nix-darwin.lib.darwinSystem {
         modules = [
           configuration
+          ./darwin.nix
           nix-homebrew.darwinModules.nix-homebrew
           {
             nix-homebrew = {
@@ -155,6 +76,12 @@
               user = "synth";
               autoMigrate = true;
             };
+          }
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.synth = import ./home.nix;
           }
         ];
       };
