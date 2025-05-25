f0b3c7008d76cd701e7316648ce80e56c61e67ec
michael <michaelcosj@proton.me>
2025-05-26 00:15:45 +0100

    feat: added hardtime.nvim for fixing any bad vim motion habits

---
+++ nix/config/nvim/lua/config/plugins/extras.lua
@@ -0,0 +1,22 @@
+-- block bad vim motions habits
+return {
+	"m4xshen/hardtime.nvim",
+	lazy = false,
+	dependencies = { "MunifTanjim/nui.nvim" },
+	opts = {
+		hints = {
+			["[dcyvV][ia][%(%)]"] = {
+				message = function(keys)
+					return "Use " .. keys:sub(1, 2) .. "b instead of " .. keys
+				end,
+				length = 3,
+			},
+			["[dcyvV][ia][%{%}]"] = {
+				message = function(keys)
+					return "Use " .. keys:sub(1, 2) .. "B instead of " .. keys
+				end,
+				length = 3,
+			},
+		},
+	},
+}
