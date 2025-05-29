4df2d16542769dae0e9fd0670f9df9d8842e2797
michael <michaelcosj@proton.me>
2025-05-29 09:28:12 +0100

    fix(lsp): json language server configuration

--- nix/config/nvim/lsp/jsonls.lua
+++ nix/config/nvim/lsp/jsonls.lua
@@ -1,11 +1,11 @@
 return {
 	cmd = {
-		"/Users/synth/.local/state/fnm_multishells/26685_1737249628581/bin/vscode-json-languageserver",
+		"/Users/synth/.local/state/fnm_multishells/26685_1737249628581/bin/vscode-json-language-server",
 		"--stdio",
 	},
 	filetypes = { "json", "jsonc" },
 	init_options = {
 		provideFormatter = true,
 	},
-	root_markers = { ".git" },
+	root_markers = { ".git", "package.json", "tsconfig.json", "jsconfig.json" },
 }
